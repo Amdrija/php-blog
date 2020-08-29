052d874b116fdd96d57a27f87bccb6f94c3a1cb6
Amdrija <andrija.jelenkovic@protonmail.com>
2020-08-29 12:40:30 +0200

    Refactoring

--- view-post.php
+++ view-post.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'lib/common.php';
+require_once 'lib/view-post.php';
+
 //get the id of the post from get parameters
 if(isset($_GET['post_id']))
 {
@@ -13,28 +15,7 @@ else
 
 // Connect to the database, run a query, handle errors
 $pdo = getPDO();
-$stmt = $pdo->prepare(
-    'SELECT
-        title, created_at, body
-    FROM
-        post
-    WHERE
-        id = :id'
-);
-if ($stmt === false)
-{
-    throw new Exception('There was a problem preparing this query');
-}
-$result = $stmt->execute(
-    array('id' => $postId, )
-);
-if ($result === false)
-{
-    throw new Exception('There was a problem running this query');
-}
-// Let's get a row
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-
+$row = getPostRow($pdo, $postId);
 
 // Swap carriage returns for paragraph breaks
 $bodyText = htmlEscape($row['body']);
