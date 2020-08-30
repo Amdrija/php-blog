61fb61a0ef0c33e1d865afee3c9f2e056526f77b
Amdrija <andrija.jelenkovic@protonmail.com>
2020-08-30 10:57:14 +0200

    Add positng comment feature

--- view-post.php
+++ view-post.php
@@ -23,10 +23,34 @@ if(!$row)
     redirectAndExit('index.php?not-found=1');
 }
 
-// Swap carriage returns for paragraph breaks
-$bodyText = htmlEscape($row['body']);
-$paraText = str_replace("\n", "</p><p>", $bodyText);
+$errors = null;
+if ($_POST)
+{
+    $commentData = array(
+        'name' => $_POST['comment-name'],
+        'website' => $_POST['comment-website'],
+        'text' => $_POST['comment-text'],
+    );
+    $errors = addCommentToPost(
+        $pdo,
+        $postId,
+        $commentData
+    );
+    // If there are no errors, redirect back to self and redisplay
+    if (!$errors)
+    {
+        redirectAndExit('view-post.php?post_id=' . $postId);
+    }
+}
+else
+{
+    $commentData = array(
+        'name' => '',
+        'website' => '',
+        'text' => '',
+    );
 
+}
 
 ?>
 <!DOCTYPE html>
@@ -47,7 +71,7 @@ $paraText = str_replace("\n", "</p><p>", $bodyText);
         </div>
         <p>
             <?php // This is already escaped, so doesn't need further escaping ?>
-            <?php echo $paraText ?>
+            <?php echo convertNewlinesToParagraphs($row['body']) ?>
 
         </p>
 
@@ -63,10 +87,12 @@ $paraText = str_replace("\n", "</p><p>", $bodyText);
                     <?php echo convertSqlDate($comment['created_at']) ?>
                 </div>
                 <div class="comment-body">
-                    <?php echo htmlEscape($comment['text']) ?>
+                    <?php // This is already escaped ?>
+                    <?php echo convertNewlinesToParagraphs($comment['text']) ?> 
                 </div>
             </div>
         <?php endforeach ?>
 
+        <?php require 'templates/comment-form.php' ?>
     </body>
 </html>
